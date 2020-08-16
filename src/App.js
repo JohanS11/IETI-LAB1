add8e7ae90eae150bc3de10809b8317c4bb44f3e
JohanS11 <53972478+JohanS11@users.noreply.github.com>
2020-08-16 13:31:26 -0500

    Adding form-3ra parte

--- src/App.js
+++ src/App.js
@@ -3,13 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 import {TodoList} from './TodoList';
 
-function App() {
-  const todos = [{text:"Learn React", priority:5, dueDate: new Date() },
-          {text:"Learn about APIs", priority:4, dueDate: new Date(2020,1,23) },
-          {text:"write TODO App", priority:3, dueDate: new Date(2020,1,30) }];
-  return (
-    <TodoList items={todos}/>
-  );
-}
+class App extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+    render(){
+      return(
+    
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
 export default App;
