5ed5ddcf7d6897ced63565cbd30445218958e43c
JohanS11 <53972478+JohanS11@users.noreply.github.com>
2020-08-16 21:51:48 -0500

    Fixing forms

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component{
 
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
+    this.state = { items: [], text: '',priority: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   };
@@ -23,9 +23,16 @@ class App extends React.Component{
             What needs to be done?
           </label>
           <input
+            name="text"
             id="new-todo"
             onChange={this.handleChange}
             value={this.state.text}
+          />
+           <input
+            name="priority"
+            id="new-todo2"
+            onChange={this.handleChange}
+            value={this.state.priority}
           />
           <button>
             Add #{this.state.items.length + 1}
@@ -36,7 +43,9 @@ class App extends React.Component{
   }
 
   handleChange(e) {
-    this.setState({ text: e.target.value });
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({ [name] : value });
   }
 
   handleSubmit(e) {
@@ -46,11 +55,13 @@ class App extends React.Component{
     }
     const newItem = {
       text: this.state.text,
-      id: Date.now()
+      priority : this.state.priority,
+      dueDate: new Date(Date.now())
     };
     this.setState(prevState => ({
       items: prevState.items.concat(newItem),
-      text: ''
+      text: '',
+      priority: ''
     }));
   }
 }
--- src/Todo.js
+++ src/Todo.js
@@ -12,7 +12,7 @@ export class Todo extends React.Component {
             <li>
                 <h2>{this.props.text}</h2>
                 <p>{this.props.priority}</p>
-                <p>{this.props.dueDate}</p>
+                <p>{this.props.dueDate.toString()}</p>
             </li>
         );
     }
